a39718062f7e7e508828616c1cad5a9a304d9a3a
Denis Shakurov <den_shak@mail.ru>
2023-06-29 22:28:28 +0300

    fix

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
+#!/bin/python3
 import socket
 import threading
+import subprocess
 
+# Connection Data
 host = '10.128.0.8'
+# host = '192.168.0.192'
 port = 55555
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 
 # Lists For Clients and Their Nicknames
 clients = []
@@ -13,32 +22,45 @@ def broadcast(message):
     for client in clients:
         client.send(message)
 
+# Ping Check
+def ping_check(ip):
+    command = ['ping', '-c', '4', ip]
+    try:
+        output = subprocess.check_output(
+            command, timeout=5, universal_newlines=True)
+        return output
+    except subprocess.CalledProcessError as e:
+        return str(e.output)
+    except subprocess.TimeoutExpired:
+        return "Ping timeout"
+
 # Handling Messages From Clients
 def handle(client):
     while True:
         try:
             # Broadcasting Messages
             message = client.recv(1024)
-            if message.decode('ascii') == 'exit':
-                remove_client(client)
-                client.close()
-                break
-            broadcast(message)
-            print("New message from " + message.decode('ascii'))
-        except:
-            # Removing And Closing Clients
-            remove_client(client)
-            client.close()
+            if message:
+                broadcast(message)
+                print("New message from " + message.decode('ascii'))
+        except ConnectionResetError:
+            # Connection Reset by Peer (client disconnected)
+            break
+        except BrokenPipeError:
+            # Broken Pipe (client disconnected)
+            break
+        except Exception as e:
+            print("Error occurred in handle:", e)
             break
 
-# Removing Client From Lists
-def remove_client(client):
+    # Removing And Closing Client
     if client in clients:
         index = clients.index(client)
         clients.remove(client)
+        client.close()
         nickname = nicknames[index]
-        nicknames.remove(nickname)
         broadcast('{} left!'.format(nickname).encode('ascii'))
+        nicknames.remove(nickname)
 
 # Receiving / Listening Function
 def receive():
@@ -53,19 +75,20 @@ def receive():
         nicknames.append(nickname)
         clients.append(client)
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
+        # Print Nickname
+        print("User {} connected".format(nickname))
+
+        # Ping Check
+        ping_result = ping_check(address[0])
+        print("Ping result for {}: {}".format(address[0], ping_result))
+
+        # Broadcast
         broadcast("{} joined!".format(nickname).encode('ascii'))
         client.send('Connected to server!'.encode('ascii'))
 
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
 
 print("Server is listening...")
 receive()
